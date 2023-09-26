c0e263dc2e6e31db074481c3c21dad8344781406
cappa <cappa@localhost.localdomain>
2023-09-26 08:23:15 -0400

    e

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 'use strict';
-const express = require('express')
+const express = require('express');
 const serverless = require('serverless-http');
-const cors = require('cors')
 
 const app = express()
-const port = 3001
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://tS7GK4PDgtoc1WqI:tS7GK4PDgtoc1WqI@cluster0.kfsxzcy.mongodb.net/?retryWrites=true&w=majority";
@@ -17,12 +15,8 @@ const client = new MongoClient(uri, {
   }
 });
 
-app.use(cors({
-    origin: '*',
-	methods: ['GET','POST']
-}))
-
-app.post('/new', async (req, res, next) => {
+const router = express.Router();
+router.post('/new', async (req, res, next) => {
 	res.set('Access-Control-Allow-Origin', '*');
 	const ipv4 = req.query.ipv4;
 	const ipv6 = req.query.ipv6;
@@ -34,6 +28,8 @@ app.post('/new', async (req, res, next) => {
 	await collection.insertMany([{"ipv4":ipv4, "ipv6":ipv6}])
 	res.send('received')
 })
+
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
